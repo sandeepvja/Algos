9d713bf8b37d89043232897be40336513b2e598f
Sandeep Mellacheruvu <mellacheruvu.sandeep@gmail.com>
2013-07-07 21:41:47 +0530

    Algorithms interface

---
+++ Algos/src/com/sandeep/dynammicprogramming/Algorithms.java
@@ -0,0 +1,11 @@
+package com.sandeep.dynammicprogramming;
+
+public interface Algorithms {
+
+	public abstract void readInput();
+
+	public abstract int optimizedSolve();
+
+	public abstract int solve();
+
+}
