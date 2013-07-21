1bc37d32511b663bdc98f276cab19a6d88e1f436
Sandeep Mellacheruvu <mellacheruvu.sandeep@gmail.com>
2013-07-21 22:27:00 +0530

    Modified to remove warning for generic type

--- Algos/src/com/sandeep/sorting/SortMain.java
+++ Algos/src/com/sandeep/sorting/SortMain.java
@@ -6,7 +6,7 @@ public class SortMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Sortable bubble = new SelectionSort();
+		Sortable<Integer> bubble = new SelectionSort<Integer>();
 		//Integer[] array = {5,3,1,4,2};
 		//Integer[] array = {5,4,3,2,1};
 		Integer[] array = {1,2,3,4,5};
